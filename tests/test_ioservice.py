958894ccfb9026374fb09ce4accb06a5ce0caf69
Bengi Mizrahi <bengimizrahi@gmail.com>
2013-02-13 17:49:05 +0200

    Implement tests.test_ioservice.TestIoService class
    This class tests basic IoService functionality. 2 IoService objects
    communicate between each other.
    
    To run the test suite, type:
    > python -m unittest tests/test_ioservice.py

---
+++ tests/test_ioservice.py
@@ -0,0 +1,38 @@
+import unittest
+
+from epc.io import IoService
+
+class TestIoService(unittest.TestCase):
+
+    def setUp(self):
+        PORT_MIN = 19040
+        self.ports = [PORT_MIN+i for i in range(2)]
+        self.ioservices = [IoService(p) for p in self.ports]
+        [s.start() for s in self.ioservices]
+
+    def test_basicMessaging(self):
+        msg1to2 = {
+            "source": "1",
+            "via": "air",
+            "protocol": "smoke",
+            "payload": {"type": "text-baloon", "content": "Anyone there?"}
+        }
+        assert self.ioservices[0].sendMessage(msg1to2, None, ("0", self.ports[1]))
+        msg1to2Prime = self.ioservices[1].getIncomingMessageQueue().get()
+        assert msg1to2 == msg1to2Prime
+        msg2to1 = {
+            "source": "2",
+            "via": "river",
+            "protocol": "bottle",
+            "payload": {"type": "paper-text", "content": "Yes, there is!"}
+        }
+        assert self.ioservices[1].sendMessage(msg2to1, "1")
+        msg2to1Prime = self.ioservices[0].getIncomingMessageQueue().get()
+        assert msg2to1 == msg2to1Prime
+    
+    def tearDown(self):
+        [s.stop() for s in self.ioservices]
+
+if __name__ == '__main__':
+    unittest.main()
+
