b890fac6d0a7b3d07c84d883a4444adfa329003b
Bengi Mizrahi <bengimizrahi@gmail.com>
2013-02-27 13:13:38 +0200

    Implement a mock-up RrcConnectionEstablishmentProcedure

---
+++ epc/procedures.py
@@ -0,0 +1,59 @@
+from messages import randomAccessRequest, rrcConnectionRequest, rrcConnectionSetupComplete
+
+class RrcConnectionEstablishmentProcedure(object):
+    
+    Success, ErrorNoRandomAccessResponse, ErrorNoRrcConnectionSetup = range(3)
+    
+    def __init__(self, maxAttempts, repeatDelay, enbAddress, ioService,
+            procedureCompleteCallback):
+        self.maxAttempts = maxAttempts # Defined as PREAMBLE_TRANS_MAX in 3GPP, this is actually read from SIB2
+        self.repeatDelay = repeatDelay
+        self.enbAddress = enbAddress
+        self.ioService = ioService
+        self.attemptNo = 0
+        self.procedureCompleteCallback = procedureCompleteCallback
+
+    def execute(self):
+        self.ioService.addIncomingMessageCallback(self.__incomingMessageCallback__)
+        self.__sendPrachPreamble__()
+    
+    def __notifyProcedureCompletion__(self, result):
+        self.ioService.removeIncomingMessageCallback(self.__incomingMessageCallback__)
+        self.procedureCompleteCallback(result)
+
+    def __incomingMessageCallback__(self, source, interface, channelInfo, message):
+        if message["messageName"] == "randomAccessResponse":
+            # assume Random Access Response is processed successfully
+            self.ioService.cancelTimer("randomAccessResponseTimeout")
+            self.__sendRrcConnectionRequest__()
+        if message["messageName"] == "rrcConnectionSetup":
+            # assume RRC Connection Setup is processed successfully
+            self.ioService.cancelTimer("rrcConnectionSetupTimeout")
+            self.__sendRrcConnectionSetupComplete__()
+        
+    def __sendPrachPreamble__(self):
+        self.attemptNo += 1
+        interface, channelInfo, message = randomAccessRequest(1, 12)
+        self.ioService.sendMessage(self.enbAddress, interface, channelInfo, message)
+        self.ioService.startTimer("randomAccessResponseTimeout", self.repeatDelay,
+            self.__onRandomAccessResponseTimeout__)
+
+    def __onRandomAccessResponseTimeout__(self, _):
+        if self.attemptNo < self.maxAttempts:
+            self.__sendPrachPreamble__()
+        else:
+            self.__notifyProcedureCompletion__(self.ErrorNoRandomAccessResponse)
+
+    def __sendRrcConnectionRequest__(self):
+        interface, channelInfo, message = rrcConnectionRequest(34343, 9989982, "moSignalling")
+        self.ioService.sendMessage(self.enbAddress, interface, channelInfo, message)
+        self.ioService.startTimer("rrcConnectionSetupTimeout", 1.0,
+            self.__onRccConnectionSetupTimeout__)
+    
+    def __onRccConnectionSetupTimeout__(self, _):
+        self.__notifyProcedureCompletion__(self.ErrorNoRrcConnectionSetup)
+    
+    def __sendRrcConnectionSetupComplete__(self):
+        interface, channelInfo, message = rrcConnectionSetupComplete(5656, "2323", {})
+        self.ioService.sendMessage(self.enbAddress, interface, channelInfo, message)
+        self.__notifyProcedureCompletion__(self.Success)
---
+++ tests/test_procedures.py
@@ -0,0 +1,49 @@
+import unittest
+import time
+
+from epc.utils.io import IoService, localhost
+from epc.procedures import RrcConnectionEstablishmentProcedure
+
+class Test_1_RrcConnectionProcedure(unittest.TestCase):
+
+    def setUp(self):
+        self.enbIoService = IoService("enb", 9000)
+        self.ueIoService = IoService("ue", 9001)
+        [s.start() for s in self.enbIoService, self.ueIoService]
+        self.procedure = RrcConnectionEstablishmentProcedure(5, 0.7, (localhost(), 9000),
+            self.ueIoService, self.__procedureCompleteCallback__)
+    
+    def tearDown(self):
+        [s.stop() for s in self.enbIoService, self.ueIoService]    
+
+    def __procedureCompleteCallback__(self, result):
+        self.result = result
+    
+    def test_1_noRandomAccessResponseReceived(self):
+        self.result = None
+        self.procedure.execute()
+        time.sleep(3.7)
+        self.assertEqual(self.result,
+            RrcConnectionEstablishmentProcedure.ErrorNoRandomAccessResponse)
+
+    def test_2_noRrcConnectionSetupReceived(self):
+        self.result = None
+        self.procedure.execute()
+        time.sleep(0.4) # smaller than 0.7
+        self.enbIoService.sendMessage("ue", None, None, {"messageName": "randomAccessResponse"})
+        time.sleep(1.5) # greater than 1.0
+        self.assertEqual(self.result,
+            RrcConnectionEstablishmentProcedure.ErrorNoRrcConnectionSetup)
+
+    def test_3_rrcConnectionEstablished(self):
+        self.result = None
+        self.procedure.execute()
+        time.sleep(0.4) # smaller than 0.7
+        self.enbIoService.sendMessage("ue", None, None, {"messageName": "randomAccessResponse"})
+        time.sleep(0.5) # smaller than 1.0
+        self.enbIoService.sendMessage("ue", None, None, {"messageName": "rrcConnectionSetup"})
+        time.sleep(0.3)
+        self.assertEqual(self.result, RrcConnectionEstablishmentProcedure.Success)
+        
+if __name__ == "__main__":
+    unittest.main()
