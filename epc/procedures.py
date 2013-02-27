a0cdf159532d162d21daacfe7ae763fb7ecfbe82
Bengi Mizrahi <bengimizrahi@gmail.com>
2013-02-27 17:41:40 +0200

    Rename some variables to increase readability
    maxAttempts -> maxPrachPreambleAttempts,
    repeatDelay -> prachPreambleRepeatDelay

--- epc/procedures.py
+++ epc/procedures.py
@@ -4,10 +4,10 @@ class RrcConnectionEstablishmentProcedure(object):
     
     Success, ErrorNoRandomAccessResponse, ErrorNoRrcConnectionSetup = range(3)
     
-    def __init__(self, maxAttempts, repeatDelay, enbAddress, ioService,
+    def __init__(self, maxPrachPreambleAttempts, prachPreambleRepeatDelay, enbAddress, ioService,
             procedureCompleteCallback):
-        self.maxAttempts = maxAttempts # Defined as PREAMBLE_TRANS_MAX in 3GPP, this is actually read from SIB2
-        self.repeatDelay = repeatDelay
+        self.maxPrachPreambleAttempts = maxPrachPreambleAttempts # Defined as PREAMBLE_TRANS_MAX in 3GPP, this is actually read from SIB2
+        self.prachPreambleRepeatDelay = prachPreambleRepeatDelay
         self.enbAddress = enbAddress
         self.ioService = ioService
         self.attemptNo = 0
@@ -38,11 +38,11 @@ class RrcConnectionEstablishmentProcedure(object):
         self.attemptNo += 1
         interface, channelInfo, message = randomAccessRequest(1, 12)
         self.ioService.sendMessage(self.enbAddress, interface, channelInfo, message)
-        self.ioService.startTimer("randomAccessResponseTimeout", self.repeatDelay,
+        self.ioService.startTimer("randomAccessResponseTimeout", self.prachPreambleRepeatDelay,
             self.__onRandomAccessResponseTimeout__)
 
     def __onRandomAccessResponseTimeout__(self, _):
-        if self.attemptNo < self.maxAttempts:
+        if self.attemptNo < self.maxPrachPreambleAttempts:
             self.__sendPrachPreamble__()
         else:
             self.__notifyProcedureCompletion__(self.ErrorNoRandomAccessResponse)
