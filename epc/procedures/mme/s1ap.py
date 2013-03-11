79c180614385600fa1f3a95e363309bd7d79642f
Bengi Mizrahi <bengimizrahi@gmail.com>
2013-03-11 16:18:42 +0200

    Fix errors in S1SetupProcedureHandler class

--- epc/procedures/mme/s1ap.py
+++ epc/procedures/mme/s1ap.py
@@ -1,31 +1,28 @@
 from ...messages.s1ap import s1SetupResponse, s1SetupFailure
 
+
 class S1SetupProcedureHandler(object):
 
-    def __init__(self, procedureParameters, enbPool, ioService):
+    def __init__(self, procedureParameters, enbPool, ioService, enbRegisteredCallback):
         self.procedureParameters = procedureParameters
-        self.enbPoolInsideMme = enbPool
+        self.enbPool = enbPool
         self.ioService = ioService
+        self.enbRegisteredCallback = enbRegisteredCallback
     
     def execute(self):
         def verifySettings():
             requiredProcedureParameters, requiredProcedureFlags = (
-                ("mmeName", "servedGummeis", "maxNumEnbsServed", "timeToWait", "flags"),
-                ("rejectS1SetupRequestsFromRegisteredEnbs")
+                ("mmeName", "servedGummeis", "timeToWait", "flags"),
+                ("rejectS1SetupRequestsFromRegisteredEnbs",)
             )
-            missingParameters, missingFlags = \
-                map(lambda items, requiredItems:
-                    filter(lambda ri: ri not in items, requiredItems), (
-                        (self.procedureParameters, requiredProcedureParameters),
-                        (self.procedureParameters["flags"], requiredProcedureFlags)
-                    )
-                )
+            missingParameters = filter(lambda p: p not in self.procedureParameters, requiredProcedureParameters)
+            missingFlags = filter(lambda f: f not in self.procedureParameters["flags"], requiredProcedureFlags)
             assert not missingParameters, "Missing parameters in mmeSettings: {}".format(missingParameters)
-            assert not missingFlags, "Missing flags in mmeSettings['flags']: {}".format(missingFlags)
+            assert not missingFlags, "Missing flags in procedureParameters['flags']: {}".format(missingFlags)
         verifySettings()
     
     def terminate(self):
-        self.ioService.stop()
+        pass
     
     def handleIncomingS1SetupMessage(self, source, interface, channelInfo, message):
         def sendReject(destination, cause, timeToWait):
@@ -35,19 +32,16 @@ class S1SetupProcedureHandler(object):
             params = (
                 self.procedureParameters["mmeName"],
                 self.procedureParameters["servedGummeis"],
-                self.procedureParameters["relativeMmeCapacity"],
-                None
+                255, None
             )
-            self.ioService.sendMessage(destination, *s1SetupResponse(*params))       
-        if self.enbPoolInsideMme.congested():
+            self.ioService.sendMessage(destination, *s1SetupResponse(*params))
+        if self.enbPool.congested():
             sendReject(source, "congestion", self.procedureParameters["timeToWait"])
             return
         globalEnbId = message["globalEnbId"]
-        if globalEnbId in self.enbPoolInsideMme and \
+        if globalEnbId in self.enbPool and \
             self.procedureParameters["flags"]["rejectS1SetupRequestsFromRegisteredEnbs"]:
                 sendReject(source, "unspecified", None)
                 return
         sendAccept(source)
-            
-        
-            
+        self.enbRegisteredCallback(globalEnbId)
