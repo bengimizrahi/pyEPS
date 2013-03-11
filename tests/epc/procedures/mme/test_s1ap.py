b188e7c23a039e220d72aea8d8bbaf6fe25eb12c
Bengi Mizrahi <bengimizrahi@gmail.com>
2013-03-11 16:19:03 +0200

    Fix errors in TestS1SetupProcedureHandler class

--- tests/epc/procedures/mme/test_s1ap.py
+++ tests/epc/procedures/mme/test_s1ap.py
@@ -4,21 +4,20 @@ import time
 from epc.utils.io import IoService, localhost
 from epc.procedures.enb.s1ap import S1SetupProcedure as EnbS1SetupProcedure
 from epc.procedures.mme.s1ap import S1SetupProcedureHandler as MmeS1SetupProcedureHandler
+from epc.nodes.mme import EnbPool
 
 
 class TestS1SetupProcedureHandler(unittest.TestCase):
 
     def setUp(self):
-        self.mmeIoService = IoService("mme", (localhost(), 9000))
+        self.mmeIoService = IoService("mme", 9000)
         self.mmeIoService.start()
-        self.enbPoolInsideMme = {}
-        self.mmeParameters = lambda maxNumEnbsServed: {
+        self.mmeParameters = {
             "mmeName": "Istanbul",
             "servedGummeis": [
                 (("28603", "28604"), (0, 1, 2), (23, 58, 127)),
                 (("00108"), (0,), (12, 13, 14)),
             ],
-            "maxNumEnbsServed": maxNumEnbsServed,
             "timeToWait": 1,
             "flags": {
                 "rejectS1SetupRequestsFromRegisteredEnbs": True
@@ -34,36 +33,49 @@ class TestS1SetupProcedureHandler(unittest.TestCase):
             "csgIdList": [],
             "defaultPagingDrx": [32, 64, 128],
         }
-        self.enbIoServices = [IoService("enb{}".format(i), (localhost(), 9001 + i)) for i in range(self.numEnbs)]
+        self.numEnbs = 2
+        self.enbIoServices = [IoService("enb{}".format(i), 9001 + i) for i in range(self.numEnbs)]
         self.enbProcedures = [
-            EnbS1SetupProcedure(self.enbProcedureParameters(i, "enb{}".format(i)), (localhost(), 9000),
-                s, self.__procedureProgressCallback__) for i, s in enumerate(self.enbIoServices)
-        ]
+            EnbS1SetupProcedure(self.enbProcedureParameters(i, "enb{}".format(i), 59900 + i),
+                (localhost(), 9000), s, self.__enbS1SetupProcedureProgressCallback__)
+            for i, s in enumerate(self.enbIoServices)]
         [s.start() for s in self.enbIoServices]
-        self.numEnbs = 100
-        self.numSuccess, self.numFail = 0, 0
+        self.numSuccess, self.numWaitingForNextAttempt = 0, 0
 
     def tearDown(self):
+        self.mmeProcedureHandler.terminate()
         self.mmeIoService.stop()
+        [p.terminate() for p in self.enbProcedures]
+        [s.stop() for s in self.enbIoServices]
         
-    def __procedureProgressCallback__(self, progress, args=None):
+    def __enbS1SetupProcedureProgressCallback__(self, progress, args=None):
         if progress == EnbS1SetupProcedure.ProgressSuccess:
             self.numSuccess += 1
-        elif progress == EnbS1SetupProcedure.ProgressFailedWithCauseUnspecified:
-            self.numFail += 1
+        elif progress == EnbS1SetupProcedure.ProgressWaitForNextAttempt:
+            self.numWaitingForNextAttempt += 1
     
-    def test_100SuccessfulS1Setups(self):
-        self.mmeProcedureHandler = MmeS1SetupProcedureHandler(self.mmeParameters(100), self.enbPoolInsideMme, self.mmeIoService)
-        self.mmeIoService.addIncomingMessageCallback(self.mmeProcedureHandler.handleIncomingS1SetupMessage)
-        self.mmeProcedureHandler.execute()
-        [p.execute() for p in self.enbProcedures]
-        time.sleep(1)
-        self.assertEqual(self.numSuccess, self.numEnbs)
+    def __mmeOnEnbRegisteredCallback__(self, globalEnbId):
+        self.enbPool.add(globalEnbId)
 
-    def test_90Successful10RejectedS1Setups(self):
-        self.mmeProcedureHandler = MmeS1SetupProcedureHandler(self.mmeParameters(90), self.enbPoolInsideMme, self.mmeIoService)
+    def test_AllSuccessfulS1Setups(self):
+        self.enbPool = EnbPool(self.numEnbs)
+        self.mmeProcedureHandler = MmeS1SetupProcedureHandler(self.mmeParameters, self.enbPool, self.mmeIoService,
+            self.__mmeOnEnbRegisteredCallback__)
         self.mmeIoService.addIncomingMessageCallback(self.mmeProcedureHandler.handleIncomingS1SetupMessage)
         self.mmeProcedureHandler.execute()
         [p.execute() for p in self.enbProcedures]
-        time.sleep(1)
-        self.assertTupleEqual((self.numSuccess, self.numFail), (90, 10))
+        time.sleep(0.9)
+        self.assertEqual(self.numSuccess, self.numEnbs)
+        self.assertEqual(len(self.enbPool), self.numEnbs)
+
+    def test_AllSuccessfulS1SetupsExceptOne(self):
+        self.enbPool = EnbPool(self.numEnbs - 1)
+        self.mmeProcedureHandler = MmeS1SetupProcedureHandler(self.mmeParameters, self.enbPool, self.mmeIoService,
+            self.__mmeOnEnbRegisteredCallback__)
+        self.mmeIoService.addIncomingMessageCallback(self.mmeProcedureHandler.handleIncomingS1SetupMessage)
+        self.mmeProcedureHandler.execute()
+        [p.execute() for p in self.enbProcedures]
+        time.sleep(0.9)
+        self.assertEqual(self.numSuccess, self.numEnbs - 1)
+        self.assertEqual(self.numWaitingForNextAttempt, 1)
+        self.assertEqual(len(self.enbPool), self.numEnbs - 1)
