9c7215244433285703a051a296a925b45c5a05fc
Bengi Mizrahi <bengimizrahi@gmail.com>
2013-03-14 14:46:32 +0200

    Fix this: time.sleep(0.2) causes test cases to fail

--- epc/procedures/enb/rrc.py
+++ epc/procedures/enb/rrc.py
@@ -40,9 +40,6 @@ class RrcConnectionEstablishmentProcedure(object):
             self.ueIdentity = message["ueIdentity"]
             self.rrcEstablishmentCause = message["rrcEstablishmentCause"]
             self.__sendContentionResolutionIdentity__(message)
-            time.sleep(0.2) # some delay before sending the next message
-            # bm: Why is this 0.2sec delay important?
-            #     [remove bm's comment after read]
             self.__sendRrcConnectionSetup__()
         if message["messageName"] == "rrcConnectionSetupComplete":
             assert message["rrcTransactionIdentifier"] == self.rrcTransactionIdentifier
